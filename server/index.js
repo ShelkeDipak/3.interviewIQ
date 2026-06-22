7a8c1c12bf89b82b4a4720918cc5e4bda3cb2a8a
Dipak Shelke <dipakshelke918@gmail.com>
2026-06-22 11:15:14 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://three-interviewiq-frontend-5b27.onrender.com",
     credentials:true
 }))
 
